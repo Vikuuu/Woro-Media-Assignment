a14ff58387145369427cb8857a3ae46e7f90019f
Aditya Vikram Singh <adivik672@gmail.com>
2023-11-26 21:20:02 +0530

    Rate Limit Added

--- myproject/settings.py
+++ myproject/settings.py
@@ -182,3 +182,14 @@ SOCIALACCOUNT_PROVIDERS = {
         "OAUTH_PKCE_ENABLED": True,
     }
 }
+
+REST_FRAMEWORK = {
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "5/minute",
+        "user": "10/minute",
+    },
+}
