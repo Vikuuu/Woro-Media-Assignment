ce831b4f808defc6817805af6fcfebf18bcbb67d
Aditya Vikram Singh <adivik672@gmail.com>
2023-11-27 15:08:49 +0530

    Caching Added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,18 +1,23 @@
 from django.urls import path
-from .views import (
-    RegistrationView, 
-    LoginView, 
-    VerifyEmailView
-)
+from .views import RegistrationView, LoginView, VerifyEmailView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from django.views.decorators.cache import cache_page
 
 
 urlpatterns = [
-    path("api/register/", RegistrationView.as_view()),
-    path('api/login/', LoginView.as_view(), name='token_obtain_pair'),
-    path('api/login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('verify-email/<str:token>/', VerifyEmailView.as_view(), name='verify-email'),
+    path("api/register/", cache_page(10)(RegistrationView.as_view())),
+    path("api/login/", cache_page(10)(LoginView.as_view()), name="token_obtain_pair"),
+    path(
+        "api/login/refresh/",
+        cache_page(10)(TokenRefreshView.as_view()),
+        name="token_refresh",
+    ),
+    path(
+        "verify-email/<str:token>/",
+        cache_page(10)(VerifyEmailView.as_view()),
+        name="verify-email",
+    ),
 ]
--- myproject/settings.py
+++ myproject/settings.py
@@ -194,3 +194,9 @@ REST_FRAMEWORK = {
     },
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.db.DatabaseCache",
+        "LOCATION": "my_cache_table",
+    }
+}
